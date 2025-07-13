715d45de1fbbc6fb071fbed72a17309e5dad182d
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-13 01:51:06 +0000

    Create main.py
    matrix difference

---
+++ matrix_difference/main.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    x = 0
+    y = 0
+    size = len(arr) - 1
+    count = 0
+    for i in zip(*arr):
+        x += i[count]
+        y += i[size - count]
+        count+=1    
+    return abs(x-y)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
