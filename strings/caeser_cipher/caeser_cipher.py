8ffdf3b796e62de3bb7380204a06446d062e105f
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-04 08:43:58 +0000

    Update caeser_cipher.py

--- strings/caeser_cipher/caeser_cipher.py
+++ strings/caeser_cipher/caeser_cipher.py
@@ -1,11 +1,23 @@
 def caesarCipher(s, k):
     # Write your code here
-    print(list(map(ord, s)))
-    if 
-    # print(list(map(lambda x:x+k,map(ord, s))))
+    print(s)
+    counter = 1
+    while k > 26:
+        counter+=1
+        k -= 26
+    print(counter)
+    cipher = list(map(ord, s))
+    f_cipher = []
+    print(k)
+    for i in cipher:
+        if (i >= 65 and i <=90) or (i >= 97 and i <= 122):
+            pass
 
-    # print("".join(map(chr, )))
-
+            
+        f_cipher.append(i)
+    out = ("".join((map(chr, f_cipher))))
+    print(out)
+    return out
 
 if __name__ == '__main__':    
 
@@ -19,3 +31,6 @@ if __name__ == '__main__':
 
     result = caesarCipher(s, k)
     result = caesarCipher("abcdefghijklmnopqrstuvwxyz", 3)
+    result = caesarCipher("There's-a-starman-waiting-in-the-sky", 3)
+    result = caesarCipher("!m-rB`-oN!.W`cLAcVbN/CqSoolII!SImji.!w/`Xu`uZa1TWPRq`uRBtok`xPT`lL-zPTc.BSRIhu..-!.!tcl!-U", 62)
+
