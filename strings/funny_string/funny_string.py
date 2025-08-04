2d612f5b53b507b53200ec3fe33a037d71ccf5ce
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-04 12:57:14 +0000

    Create funny_string.py
    funny_string

---
+++ strings/funny_string/funny_string.py
@@ -0,0 +1,20 @@
+def funnyString(s):    
+    s = list(map(ord, s))
+    rev_s = []    
+
+    for i in range(len(s)):
+        x = s[i:i+2]
+        abdiff = abs(x[-1]- x[0])
+        if abdiff == 0:
+            continue
+        rev_s.append(abdiff)
+    if (rev_s == rev_s[::-1]):
+        return "Funny"
+    return "Not Funny"
+
+
+if __name__ == '__main__':
+    funnyString("acxz")
+    funnyString("bcxz")
+    funnyString("lmnop")
+    
