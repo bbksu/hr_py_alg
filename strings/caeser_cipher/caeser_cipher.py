11c86cbf6578ef93e189655748ba58b52dd309fd
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-04 06:28:16 +0000

    Create caeser_cipher.py
    caeser_cipher

---
+++ strings/caeser_cipher/caeser_cipher.py
@@ -0,0 +1,21 @@
+def caesarCipher(s, k):
+    # Write your code here
+    print(list(map(ord, s)))
+    if 
+    # print(list(map(lambda x:x+k,map(ord, s))))
+
+    # print("".join(map(chr, )))
+
+
+if __name__ == '__main__':    
+
+    # n = int(input().strip())
+
+    # s = input()
+    s = "middle-Outz"
+
+    # k = int(input().strip())
+    k = 2
+
+    result = caesarCipher(s, k)
+    result = caesarCipher("abcdefghijklmnopqrstuvwxyz", 3)
