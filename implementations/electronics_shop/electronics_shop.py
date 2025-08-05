d6a163432e826d16ed6a993e47e9ab4af005663d
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-05 06:09:42 +0000

    Update electronics_shop.py
    fix

--- implementations/electronics_shop/electronics_shop.py
+++ implementations/electronics_shop/electronics_shop.py
@@ -2,9 +2,9 @@ from itertools import product
 
 def getMoneySpent(keyboards, drives, b):
     cart_product = product(keyboards, drives)
-    price = map(sum, cart_product)
-    cost = [i if i < b else -1  for i in price]    
-    cost = max(cost)
+    price = map(sum, cart_product)  
+    cost = [i if i <= b else -1  for i in price]    
+    cost = max(cost)    
     return (cost)
         
 
