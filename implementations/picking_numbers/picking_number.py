53b65b6c2d4dfcb806897c7067015a9541e1def2
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-05 11:32:24 +0000

    Update picking_number.py

--- implementations/picking_numbers/picking_number.py
+++ implementations/picking_numbers/picking_number.py
@@ -1,23 +1,26 @@
-from itertools import combinations, product
-def pickingNumbers(a):
-    # Write your code here
-    a.sort()    
+from itertools import combinations
+def pickingNumbers(a):    
     combs = list(combinations(a, 2))
-    comb_dict = dict()
-    print(combs)    
-    for i in combs:
-        comb_dict[i] = comb_dict.setdefault(i, 0)+1
-    max_val = max(comb_dict.values())
-
-    for i, y in comb_dict.items():
-        if y == max_val:
-            print(i)
-
-    print(comb_dict)           
+    comb_dict = dict()    
+    for i, x in combs:
+        if abs(i -x) == 1:                        
+            comb_dict[x, i] = comb_dict.setdefault((x, i),0)+1
+    rep = max(comb_dict.values())
+    common = []
+    for i, x in comb_dict.items():
+        if x == rep:
+            common = list(i)
+            break
+    final = 0
+    for i in common:
+        final += a.count(i)    
+    return (final)
+    
 
 
 
 if __name__ == '__main__':    
 
     result = pickingNumbers([4,6,5,3,3,1])
-    # result = pickingNumbers([1, 1, 2,2,4,4,5,5,5])
+    result = pickingNumbers([1, 1, 2,2,4,4,5,5,5])
+    result = pickingNumbers([1,2,2,3,1,2])
