f18f1ec21c81578997f5244a7834f595cdab503d
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-05 05:56:34 +0000

    Create electronics_shop.py
    get_money_spent

---
+++ implementations/electronics_shop/electronics_shop.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+def getMoneySpent(keyboards, drives, b):
+    cart_product = product(keyboards, drives)
+    price = map(sum, cart_product)
+    cost = [i if i < b else -1  for i in price]    
+    cost = max(cost)
+    return (cost)
+        
+
+if __name__ == '__main__':    
+    moneySpent = getMoneySpent([3, 1], [5, 2, 8], 10)
