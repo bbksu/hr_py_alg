3362a19581de72605e8256d8b956be6d367f21b6
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-06 11:27:25 +0000

    Update angry_professor.py
    fix

--- implementations/angry_professor/angry_professor.py
+++ implementations/angry_professor/angry_professor.py
@@ -1,10 +1,11 @@
-def angryProfessor(k, a):
-    # Write your code here
-    late = [max(0, i) for i in k]
-    print(late, late.count(0))
+def angryProfessor(k, a):    
+    late = [max(0, i) for i in a]    
+    if late.count(0) < k:
+        return ("YES")
+    return ("NO")
     
 
 if __name__ == '__main__':   
-    result = angryProfessor([-1,-3,4,2], 3)
-    result = angryProfessor([0,-1,2,1], 2)
-    result = angryProfessor([-2,-1,0,1,2 ], 3)
+    result = angryProfessor( 3, [-1,-3,4,2])
+    result = angryProfessor(2, [0,-1,2,1])
+    result = angryProfessor(3, [-2,-1,0,1,2 ])
