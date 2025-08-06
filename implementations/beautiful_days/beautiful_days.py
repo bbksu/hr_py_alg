d8f6b6f9ae47e603e7ec6bf92b5f4924ee6bec9a
Unknown <87952662+bbksu@users.noreply.github.com>
2025-08-06 22:02:09 +0000

    Update beautiful_days.py

--- implementations/beautiful_days/beautiful_days.py
+++ implementations/beautiful_days/beautiful_days.py
@@ -1,5 +1,9 @@
-def beautifulDays(i, j, k):    
-    pass
+def beautifulDays(i, j, k):
+    for i in range(i, j+1):
+        day = f"({i}-{int(str(i)[::-1])})/{k}"        
+        day = (abs(eval(day)))
+        if day == int(day):
+            print(i)
 
 if __name__ == '__main__':
     result = beautifulDays(20, 23, 6)
